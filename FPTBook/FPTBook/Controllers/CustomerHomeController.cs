bad547feb3a282ac9eaf8a5a579344c6d678c85d
@nv_truonqqq <75243419+truongnvgcd201597@users.noreply.github.com>
2023-04-04 15:39:34 +0700

    Add Index.cshtml viewpages for all controller

--- FPTBook/FPTBook/Controllers/CustomerHomeController.cs
+++ FPTBook/FPTBook/Controllers/CustomerHomeController.cs
@@ -6,22 +6,9 @@ namespace FPTBook.Controllers
 {
     public class CustomerHomeController : Controller
     {
-        private readonly ILogger<CustomerHomeController> _logger;
-
-        public CustomerHomeController(ILogger<CustomerHomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
