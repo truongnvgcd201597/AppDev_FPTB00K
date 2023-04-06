b23fdb69c01307f7236295eb105b2f2860e05afb
@nv_truonqqq <75243419+truongnvgcd201597@users.noreply.github.com>
2023-04-06 12:14:28 +0700

    errorr

---
+++ FPTBook/FPTBook/FPTBook/Controllers/CustomerAccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBook.Controllers
+{
+    public class CustomerAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ FPTBook/FPTBook/FPTBook/Controllers/CustomerHomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FPTBook.Models
+{
+    public class CustomerHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult FAQ() { 
+            return View();
+        }
+    }
+}
