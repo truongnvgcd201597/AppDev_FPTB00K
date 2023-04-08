10d2d459e595bad2907bbefa202e23588eb967da
@nv_truonqqq <75243419+truongnvgcd201597@users.noreply.github.com>
2023-04-08 13:04:08 +0700

    Revise json cofig database error

--- FPTBook/FPTBook/FPTBook/Program.cs
+++ FPTBook/FPTBook/FPTBook/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
