c317a993260f7d2096823c5100b45604a909adc0
hoductan <tanhdgcd201919@fpt.edu.vn>
2023-04-14 09:19:55 +0700

    edit homecontroller/book view/book/index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using FPTBook.Data;
 using Microsoft.AspNetCore.Mvc;
 using FPTBook.Models;
 using Microsoft.EntityFrameworkCore;
+using FPTBook.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FPTBook.Controllers;
 
@@ -11,16 +13,28 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly ApplicationDbContext _dbContext;
+    private ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+    public HomeController(ApplicationDbContext db)
     {
-        _logger = logger;
-        _dbContext = dbContext;
+        _db = db;
     }
+
+   
     
     public async Task<IActionResult> Index()
     {
-       return View();
+        var booksListAsync = await _db.Books.Include(c => c.Category).ToListAsync();
+        var categoriesName = from n in _db.Categories
+                             select n.Name;
+        var categoryName = await _db.Categories.Select(b => b.Name).ToListAsync();
+        BookCategoryViewModels bookCategory = new BookCategoryViewModels()
+        {
+            Books = booksListAsync,
+            Categories = new SelectList(categoryName)
+        };
+        return View(bookCategory);
+        
     }
 
     [AutoValidateAntiforgeryToken]
