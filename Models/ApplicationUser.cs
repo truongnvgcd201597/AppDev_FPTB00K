e5733252656fe0f0dec4fcdec416c779225609db
@nv_truonqqq <75243419+truongnvgcd201597@users.noreply.github.com>
2023-04-24 20:03:52 +0700

    Update models section
    Co-Authored-By: phannhatkhoa <106651601+phannhatkhoa@users.noreply.github.com>
    Co-Authored-By: hoductan <106651892+hoductan@users.noreply.github.com>

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,17 +1,16 @@
-using System.Security.Claims;
-using FPTBook.Data;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace FPTBook.Models;
 
 public class ApplicationUser : IdentityUser
 {
     [PersonalData]
+    [StringLength(50)]
     public string Fullname { get; set; }
+
     [PersonalData]
+    [StringLength(250)]
     public string HomeAddress { get; set; }
-    /*[PersonalData]
-    public Role Roles { get; set; }*/
-    
-    
-}      
+}
